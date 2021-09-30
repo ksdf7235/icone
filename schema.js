58bf1571ab5dbfb94044723f03f959ef921afc85
ksdf7235 <85724877+ksdf7235@users.noreply.github.com>
2021-09-30 14:56:02 +0900

    0930
    0930

--- schema.js
+++ schema.js
@@ -1,14 +1,7 @@
-import {loadFilesSync,mergeTypeDefs,mergeResolvers,makeExecutableSchema} from "graphql-tools"
-
-
+import { loadFilesSync, mergeResolvers, mergeTypeDefs } from "graphql-tools";
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-const loadedResolver = loadFilesSync(`${__dirname}/**/*.{queries,mutation}.js`);
-const typeDefs = mergeTypeDefs(loadedTypes);
-const resolvers = mergeResolvers(loadedResolver);
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
 
-
-const schema = makeExecutableSchema({typeDefs,resolvers})
-   
-
-export default schema;
+export const typeDefs = mergeTypeDefs(loadedTypes);
+export const resolvers = mergeResolvers(loadedResolvers);
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
 import {ApolloServer } from "apollo-server";
-import schema from "./schema";
+import {typeDefs,resolvers} from "./schema.js";
+import { getUser,protectResolver } from "./users/users.utils";
 
 
 
 const PORT = process.env.PORT
 
 const server = new ApolloServer({
-    schema,
+    resolvers,
+    typeDefs,
+    context: async({req}) => {
+       
+        return {
+            loggedInUser: await getUser(req.headers.token),
+            protectResolver
+          
+        };
+    },
 });
 
 server.listen(PORT).then(() => console.log(`Server is running on http://localhost:${PORT}/`))
